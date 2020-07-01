785a992ca0ef87d793d32f778c14969f688b4c3a
Ruchir Rathore <rathoreruchir7@gmail.com>
2020-07-01 19:13:13 +0530

    commit 10

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -15,6 +15,7 @@ return(
         <div className="row row-content">
             <div className="col-12 col-md-6">
                 <h2>About</h2>
+                <hr />
                 <p>The platform provides users to buy and sellers to sell their products and make their shopping easy.</p>
                 
             </div>
