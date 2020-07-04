924a8a9b27a1819562e52dd58491e3fa925c5bef
Ruchir Rathore <rathoreruchir7@gmail.com>
2020-07-04 15:49:34 +0530

    changed the login route from preventdefault to hom page

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,8 +64,8 @@ class Header extends Component{
       console.log(errorMessage);
     });
     console.log(auth.currentUser);
-    event.preventDefault();
-    // this.props.history.push('/home');
+  
+    this.props.history.push('/home');
  }
 
  googleSignInHandle(){
