d11833a18f74d81b8bf3948938df911c2dc35c62
Ruchir Rathore <rathoreruchir7@gmail.com>
2020-07-06 12:57:45 +0530

    changed the button onclick function}

--- src/components/SuccessComponent.js
+++ src/components/SuccessComponent.js
@@ -7,9 +7,14 @@ class Success extends Component{
     constructor(props)
     {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
-    onClick()
+    componentDidMount()
     {
+        window.scrollTo(0,0);
+    }
+    handleClick()
+    {   console.log('hello');
         this.props.history.push('/menu');
     }
     render()
@@ -20,7 +25,7 @@ class Success extends Component{
             <Image src = 'https://firebasestorage.googleapis.com/v0/b/newproject-f4730.appspot.com/o/images2%2FWeb%201920%20%E2%80%93%201.png?alt=media&token=23b03fba-dedc-4a06-8e49-c39eb8a1fecf' fluid /></div>
             <div className='row'>
                 <div className='col-12 col-md-3 offset-3 offset-md-5 mt-1'>
-            <Button  color='primary' onClick={() => this.onClick} >Go Back To Menu</Button>
+            <Button  color='primary' onClick={this.handleClick} >Go Back To Menu</Button>
             </div>
             </div>
             </div>
@@ -28,4 +33,4 @@ class Success extends Component{
     }
 }
 
-export default Success;
+export default withRouter(Success);
